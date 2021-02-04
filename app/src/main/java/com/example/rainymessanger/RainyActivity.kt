ef0cbb48ea18332b59bf4fc577c9c7bc760f9712
hosseinkasiri <hosseinkasiri5@gmail.com>
2021-02-04 16:00:25 +0330

    design welcome page

---
+++ app/src/main/java/com/example/rainymessanger/RainyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rainymessanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RainyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rainy)
+    }
+}
